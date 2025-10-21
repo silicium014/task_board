f47963de04add9ae3fa82788c81b17cf0fe2d6c8
silicium014 <perovoart@gmail.com>
2025-10-21 04:10:36 +0300

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapping: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: [
+    '**/tests/**/*.(ts|tsx)',
+    '**/*.(test|spec).(ts|tsx)'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/main.tsx',
+    '!src/vite-env.d.ts',
+  ],
+};
